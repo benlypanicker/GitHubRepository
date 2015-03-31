f76ae0579c1febc35469b1263fa37983585a9969
Benly Panicker <benly.panicker@htcindia.com>
2015-03-31 16:51:19 +0530

    Revert "First Commit"
    This reverts commit 0c167935b2c226dda702de312d6514f0d361c01d.

--- app/details/Flight_flightDetails_FlightDetails.controller.js
+++
@@ -1,9 +0,0 @@
-sap.ui.controller("app.details.Flight_flightDetails_FlightDetails", {
-
-	onBeforeShow : function(oData) {
-		this.getView().bindElement(oData.bindingContext.getPath());
-   	},
-    
-	onNavButtonTap : function() {
-		sap.ui.getCore().getEventBus().publish("nav", "back");
-	} });
